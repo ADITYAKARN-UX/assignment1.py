4ada59c4a77cb14a8688fbebad570e80f148e9af
ADITYAKARN-UX <kk0058700@gmail.com>
2025-07-02 17:40:34 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+# task 1
+# a=int(input("Enter the first number: "))
+# b=int(input("Enter the second number: "))
+# print("Addition:",a+b)
+# print("Subtraction:",a-b)
+# print("Multiplication:",a*b)
+# print("Division:",a/b)
+
+#=============================================
+
+
+# task 2
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+print("Hello, " + a + " " + b  + "! " + " Welcome " + " to " " the "+ " Python "+" program. ")
